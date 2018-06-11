2727f7fbd6d39ea1d565b7bc54a3469ea7039cbf
JM. Hiltbrunner <jm.hiltbrunner@gmail.com>
2018-06-11 17:39:01 +0200

    Ajout des getters/setters et constructeurs

---
+++ model/Personnage.php
@@ -0,0 +1,93 @@
+<?php
+
+class Personnage {
+    //Attributes
+    private $_force = 0;
+    private $_localisation  = '';
+    private $_experience = 0;
+    private $_degats = 0;
+
+    
+    // Getters
+    public function experience() {
+        return $this -> _experience;
+    }
+    
+    public function degats() {
+        return $this -> _degats;
+    }
+    
+    public function localisation() {
+        return $this -> _localisation;
+    }
+    
+    public function force() {
+        return $this -> _force;
+    }
+    
+    // Setters
+    public function setForce($force){
+        //Test pour vérifier si le paramètre entier est un entier
+        if(!is_int($force)) {
+            trigger_error('La force doit être un nombre entier. Saississez une valeur entière', E_USER_WARNING);
+            return;
+        }
+        else {
+            $this -> _force = $force;
+        }
+    }
+    
+    public function setLocalisation($localisation){
+        //Test pour vérifier si le paramètre entier est un entier
+        if(!is_string($localisation)) {
+            trigger_error('La localisation doit être un texte. Saississez une ville non numérique', E_USER_WARNING);
+            return;
+        }
+        else {
+            $this -> _localisation = $localisation;
+        }
+    }
+    
+      public function setExperience($experience){
+        //Test pour vérifier si le paramètre entier est un entier
+        if(!is_int($experience) and $experience <= 100) {
+            trigger_error('L\'expérience doit être un nombre entier inférieur ou égal à 100. Saississez une valeur entière', E_USER_WARNING);
+            return;
+        }
+        else {
+            $this -> _experience = $experience;
+        }
+    }
+    public function setDegats($degats){
+        //Test pour vérifier si le paramètre entier est un entier
+        if(!is_int($degats) and $degats <= 100) {
+            trigger_error('L\'expérience doit être un nombre entier inférieur ou égal à 100. Saississez une valeur entière', E_USER_WARNING);
+            return;
+        }
+        else {
+            $this -> _degats = $degats;
+        }
+    }
+    
+     //Constructor
+    public function __construct($force = 0, $localisation = '', $degats = 0) {
+        $this -> setForce($force);
+        $this -> setLocalisation($localisation);
+        $this -> setDegats($degats);
+        $this -> _experience = 0;
+    }
+    
+    //Methods
+    public function deplacer() {
+        
+    }
+    
+    public function frapper(Personnage $personnage) {
+        $personnage -> _degats += $this -> _force;
+    }
+    
+    public function gagnerExperience() {
+        $this -> _experience += 1;
+    }
+    
+}
---
+++ model/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+function chargerClasse($classe){
+    require $classe.'.php';
+}
+
+spl_autoload_register('chargerClasse');
+
+$heros = new Personnage(20, 'La Bourboule', 0);
+$victime = new Personnage(5, 'Mulhouse', 25);
+echo 'Personnage héros de force '.$heros -> force().', de localisation '.$heros -> localisation().' et ayant '.$heros -> degats().' degats subis';
+for($i=1; $i<11; $i++) {
+    echo $heros -> experience();
+    $heros -> frapper($victime);
+    $heros -> gagnerExperience();
+    echo '<p/>';
+}
+echo $victime -> degats();

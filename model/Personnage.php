0a0ecf27a4bac7a8005e44729a08e21f9e1e210b
jm.hiltbrunner <jm.hiltbrunner@UM.Viveris.lan>
2018-06-12 10:51:58 +0200

    Ajout de l'hydratation version longue

--- model/Personnage.php
+++ model/Personnage.php
@@ -1,11 +1,14 @@
 <?php
+namespace org\formation\php\model;
 
 class Personnage {
     //Attributes
-    private $_force = 0;
-    private $_localisation  = '';
+    private $_id='';
+    private $_name="";
+    private $_strength = 0;
+    private $_place  = '';
     private $_experience = 0;
-    private $_degats = 0;
+    private $_damage = 0;
     
     //Contants attributes
     const FORCE_PETITE = 25;
@@ -17,59 +20,87 @@ class Personnage {
 
     
     // Getters
+    public function id() {
+        return $this->_id;
+    }
+    
+    public function name(){
+        return $this->_name;
+    }
+    
     public function experience() {
         return $this -> _experience;
     }
     
-    public function degats() {
-        return $this -> _degats;
+    public function damage() {
+        return $this -> _damage;
     }
     
-    public function localisation() {
-        return $this -> _localisation;
+    public function place() {
+        return $this -> _place;
     }
     
-    public function force() {
-        return $this -> _force;
+    public function strength() {
+        return $this -> _strength;
     }
     
     // Setters
-    public function setForce($force){
-        
-        if(in_array($force, [self::FORCE_PETITE, self::FORCE_MOYENNE, self::FORCE_GRANDE])) {
-            $this -> _force = $force;
+    public function setId($id) {
+        $idCast = (int) $id;
+        if ($idCast > 0) {
+            $this->_id = $idCast;
+        }
+        else {
+            trigger_error('L\'identifiant doit être un nombre positif. Vérifiez la saisie.', E_USER_WARNING);
         }
     }
     
-    public function setLocalisation($localisation){
+    public function setName($name) {
+        if(is_string($name)) {
+            $this->_name = $name;
+        }
+        else {
+            trigger_error('Le nom doit être une chaîne de caractères. Vérifiez la saisie', E_USER_WARNING);
+        }
+    }
+    
+    public function setStrength($strength){
+        
+        if(in_array($strength, [self::FORCE_PETITE, self::FORCE_MOYENNE, self::FORCE_GRANDE])) {
+            $this -> _strength = $strength;
+        }
+    }
+    
+    public function setPlace($place){
         //Test pour vérifier si le paramètre entier est un entier
-        if(!is_string($localisation)) {
-            trigger_error('La localisation doit être un texte. Saississez une ville non numérique', E_USER_WARNING);
+        if(!is_string($place)) {
+            trigger_error('La place doit être un texte. Saississez une ville non numérique', E_USER_WARNING);
             return;
         }
         else {
-            $this -> _localisation = $localisation;
+            $this -> _place = $place;
         }
     }
     
       public function setExperience($experience){
         //Test pour vérifier si le paramètre entier est un entier
-        if(!is_int($experience) and $experience <= 100) {
-            trigger_error('L\'expérience doit être un nombre entier inférieur ou égal à 100. Saississez une valeur entière', E_USER_WARNING);
+        $xp = (int) $experience;
+          if($xp<0 or $xp > 100) {
+            trigger_error('L\'expérience doit être un nombre entier inférieur compris entre 0 et 100. Saississez une valeur entière', E_USER_WARNING);
             return;
         }
         else {
-            $this -> _experience = $experience;
+            $this -> _experience = $xp;
         }
     }
-    public function setDegats($degats){
+    public function setDamage($damage){
         //Test pour vérifier si le paramètre entier est un entier
-        if(!is_int($degats) and $degats <= 100) {
+        if(!is_int($damage) and $damage <= 100) {
             trigger_error('L\'expérience doit être un nombre entier inférieur ou égal à 100. Saississez une valeur entière', E_USER_WARNING);
             return;
         }
         else {
-            $this -> _degats = $degats;
+            $this -> _damage = $damage;
         }
     }
     
@@ -78,24 +109,54 @@ class Personnage {
     }
     
      //Constructor
-    public function __construct($force = 0, $localisation = '', $degats = 0) {
-        $this -> setForce($force);
-        $this -> setLocalisation($localisation);
-        $this -> setDegats($degats);
+    public function __construct($name='', $strength = 0, $place = '', $damage = 0) {
+        $this ->setName($name);
+        $this -> setStrength($strength);
+        $this -> setLocalisation($place);
+        $this -> setDamage($damage);
         $this -> _experience = 0;
     }
     
     //Methods
-    public function deplacer() {
-        
+    public function move($newPlace) {
+        $this->setPlace($newPlace);
+        echo $this ->_name.' has moved to '.$this->_place;
     }
     
     public function frapper(Personnage $personnage) {
-        $personnage -> _degats += $this -> _force;
+        $personnage -> _damage += $this -> _strength;
+        echo $personnage->_name.' has received '.$this->_damage.' by '.$this->_name;
     }
     
     public function gagnerExperience() {
-        $this -> _experience += 1;
+        $this->setExperience($this -> _experience++);
+        echo $this->_name.' has '.$this->_experience.' experience points.';
+    }
+    
+    public function hydate(array $data) {
+        if (isset($data['id'])) {
+            $this->setId($data['id']);
+        }
+        
+        if (isset($data['name'])) {
+            $this->setName($data['name']);
+        }
+        
+        if (isset($data['strength'])) {
+            $this->setStrength($data['strength']);
+        }
+        
+        if (isset($data['place'])) {
+            $this->setPlace($data['place']);
+        }
+        
+        if (isset($data['damage'])) {
+            $this->setDamage($data['damage']);
+        }
+        
+        if (isset($data['experience'])) {
+            $this->setExperience($data['experience']);
+        }
     }
     
 }
